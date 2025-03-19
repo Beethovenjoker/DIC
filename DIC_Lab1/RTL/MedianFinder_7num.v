64589c89084aa3c5e1e5c144f6e4e9752254942d
Beethovenjoker <c11544211690@gmail.com>
2025-03-20 01:46:13 +0800

    Update MedianFinder_7num.v

--- DIC_Lab1/RTL/MedianFinder_7num.v
+++ DIC_Lab1/RTL/MedianFinder_7num.v
@@ -1,12 +1,10 @@
-module MedianFinder_7num(
-    input  	[3:0]  	num1  , 
-	input  	[3:0]  	num2  , 
-	input  	[3:0]  	num3  , 
-	input  	[3:0]  	num4  , 
-	input  	[3:0]  	num5  , 
-	input  	[3:0]  	num6  , 
-	input  	[3:0]  	num7  ,  
-    output 	[3:0] 	median  
+module MedianFinder_5num(
+    input  [3:0] 	num1  , 
+	input  [3:0] 	num2  , 
+	input  [3:0] 	num3  , 
+	input  [3:0] 	num4  , 
+	input  [3:0] 	num5  ,  
+    output [3:0] 	median  
 );
 
     Comparator2 com_1(
@@ -17,83 +15,32 @@ module MedianFinder_7num(
     );
 
     Comparator2 com_2(
-        .A(com_1.max),
-        .B(num3),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_3(
-        .A(com_2.max),
+        .A(num3),
         .B(num4),
         .min(),
         .max()
     );
 
-    Comparator2 com_4(
-        .A(com_3.max),
-        .B(num5),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_5(
-        .A(com_4.max),
-        .B(num6),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_6(
-        .A(com_5.max),
-        .B(num7),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_a(
-        .A(com_5.min),
-        .B(com_6.min),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_b(
-        .A(com_4.min),
-        .B(com_a.min),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_c(
-        .A(com_3.min),
-        .B(com_b.min),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_d(
-        .A(com_2.min),
-        .B(com_c.min),
-        .min(),
-        .max()
-    );
-
-    Comparator2 com_e(
+    Comparator2 com_3(
         .A(com_1.min),
-        .B(com_d.min),
+        .B(com_2.min),
         .min(),
         .max()
     );
 
-    MedianFinder_5num median_5(
-		.num1(com_a.max),
-		.num2(com_b.max),
-		.num3(com_c.max),
-		.num4(com_d.max),
-		.num5(com_e.max),
-		.median()
-	);
+    Comparator2 com_4(
+        .A(com_1.max),
+        .B(com_2.max),
+        .min(),
+        .max()
+    );
 
-    assign median = median_5.median;
+    MedianFinder_3num med3(
+		.num1(com_3.max),
+		.num2(com_4.min),
+		.num3(num5),
+		.median()
+    );
+
+    assign median = med3.median;
 endmodule
