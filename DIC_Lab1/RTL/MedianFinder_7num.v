67f89c93b35d2d45b8b33d8823f48ac5717bdcde
Beethovenjoker <c11544211690@gmail.com>
2025-03-20 01:52:07 +0800

    Update MedianFinder_7num.v

--- DIC_Lab1/RTL/MedianFinder_7num.v
+++ DIC_Lab1/RTL/MedianFinder_7num.v
@@ -1,10 +1,10 @@
 module MedianFinder_5num(
-    input  [3:0] 	num1  , 
-	input  [3:0] 	num2  , 
-	input  [3:0] 	num3  , 
-	input  [3:0] 	num4  , 
-	input  [3:0] 	num5  ,  
-    output [3:0] 	median  
+    input  [3:0] num1, 
+    input  [3:0] num2, 
+    input  [3:0] num3, 
+    input  [3:0] num4, 
+    input  [3:0] num5,  
+    output [3:0] median  
 );
 
     Comparator2 com_1(
